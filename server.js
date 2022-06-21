9c30944b502b4fcbd878ee5c6f57915dfb14ab34
BigSuggs72 <msofi72@gmail.com>
2022-06-21 07:44:26 -0400

    first commit

---
+++ server.js
@@ -0,0 +1,82 @@
+//MayanWolfe VOD at 3:00 pm on 6/19/2022: Let's Make an Auto-Completing Movie Finder App #100Devs
+
+const express = require('express')
+const app = express()
+const cors = require('cors')
+const {MongoClient, ObjectId } = require('mongodb')
+require('dotenv').config()
+const PORT = 8000
+
+
+//DECLARED DB VARIABLES(Hide credentials)
+let db,
+    dbConnectionStr = process.env.DB_STRING,
+    dbName = 'sample_mflix',
+    collection
+
+
+// CONNECT TO MONGO DB    
+MongoClient.connect(dbConnectionStr)
+    .then(client => {
+        console.log(`Connected to the MongoDB ${dbName} database.`)
+        db = client.db(dbName)
+        collection = db.collection('movies')
+    })
+
+
+//SET MIDDLEWARE - 1:19:00
+app.use(express.urlencoded({extended: true}))
+app.use(express.json())
+app.use(cors())
+
+
+
+//CRUD  METHODS - 1:22:00
+app.get('/search', async (req, res) => {
+    try{
+        let result = await collection.aggregate([
+            {
+                "$Search" : {
+                    "autocomplete" : {
+                        "query": `${request.query.query}`,
+                        "path": "title",
+                        "fuzzy": {
+                            "maxEdits":2,
+                            "prefixLength": 3
+                        }
+                    }
+                }
+            }
+        ]).toArray()
+            response.send(result)
+    } catch (error) {
+        response.status(500).send({message: error.message})
+    }
+})
+
+
+app.get('/get/:id', async (req, res) => {
+    try {
+        let result = await collection.findOne({
+            "_id" : ObjectId(req.params.id)
+        })
+        response.send(result)
+    } catch (error) {
+        response.status(500).send({message: error.message})
+    }
+})
+
+
+
+
+
+
+
+
+
+
+//SET UP LOCALHOST ON PORT
+app.listen(process.env.port || PORT, () => {
+    console.log(`The moviesearch server is active on port ${PORT}!!!`)
+})
+
